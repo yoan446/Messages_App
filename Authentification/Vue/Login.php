1591ef8bb110cede65354a1c629357e02327c692
yoan446 <azebazebrayan@gmail.com>
2025-02-15 21:56:15 +0100

    Mise en place architecture de branche Authentification

---
+++ Authentification/Vue/Login.php
@@ -0,0 +1,14 @@
+<?php
+//code php pour le controller
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connection</title>
+</head>
+<body>
+    
+</body>
+</html>
