de663e84818952d9dcaacc31bf3548b1a1e2c918
yoan446 <azebazebrayan@gmail.com>
2025-02-26 11:13:13 +0100

    backend numero 3

--- Interface_message/Controlleur/charger_contact.php
+++ Interface_message/Controlleur/charger_contact.php
@@ -1,2 +1,11 @@
 <?php
+// Démarrer la session si ce n'est pas déjà fait
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+//connection a la BDD
+include('../Modele/connection.php');
+
+$sql="";
 ?>
